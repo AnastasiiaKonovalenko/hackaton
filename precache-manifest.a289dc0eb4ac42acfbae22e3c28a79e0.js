e860906a79f6b26450273f06c7e8b3b32f35ddab
ksukhaldun <kseniya.khaldun@gmail.com>
2020-01-19 09:25:36 +0300

    Updates

---
+++ precache-manifest.a289dc0eb4ac42acfbae22e3c28a79e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7e8d4146f7bf38ac6aa41bbc15e1d8d",
+    "url": "//hackaton.git/index.html"
+  },
+  {
+    "revision": "5c672de0b0aab10c8e36",
+    "url": "//hackaton.git/static/css/main.bd661d22.chunk.css"
+  },
+  {
+    "revision": "b829692f51f58c98b1bd",
+    "url": "//hackaton.git/static/js/2.d29b3332.chunk.js"
+  },
+  {
+    "revision": "5c672de0b0aab10c8e36",
+    "url": "//hackaton.git/static/js/main.c778ea1d.chunk.js"
+  },
+  {
+    "revision": "9ceebfa33dba3ccd219d",
+    "url": "//hackaton.git/static/js/runtime~main.e68dab4c.js"
+  }
+]);
